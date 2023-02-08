07696292964d6819c8f3271f17a5ebddca61b100
Wiktor <wiktorzielak@gmail.com>
2023-02-08 14:46:51 +0100

    responsive navigation is working

---
+++ JustChat/wwwroot/js/handleHamburgerBtn.js
@@ -0,0 +1,11 @@
+﻿let handleHamburgerBtn = function () {
+    let hamburgerBtn = document.querySelector(".hamburger");
+    let navItems = document.querySelectorAll(".nav-item");
+    let nav = document.querySelector(".nav");
+    hamburgerBtn.addEventListener('click', function () {
+        nav.classList.toggle('nav-small');
+        navItems.forEach(function (item) {
+            item.classList.toggle('collapse');
+        })
+    });
+}
