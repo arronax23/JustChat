51ac035c3125d34b609c66134504494afada3397
Wiktor <wiktorzielak@gmail.com>
2023-02-16 14:30:34 +0100

    changed connection string, now is using user/password credentials instead of windows authentication, which makes it better for IIS deployment

--- JustChat/Startup.cs
+++ JustChat/Startup.cs
@@ -53,16 +53,16 @@ namespace JustChat
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            //if (env.IsDevelopment())
+            //{
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
